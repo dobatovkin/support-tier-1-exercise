d7ec508ad0ab17d49dbf9934046f3f921d096b17
Yahor Dabatoukin <yahor.dabatoukin@mapbox.com>
2024-03-06 19:10:42 +0300

    comment task 1

--- task1.js
+++ task1.js
@@ -3,12 +3,16 @@ This task implies that you should parse this JSON file and output the value of "
 And then from this data to build a new JSON file with only the values: "name", "price", "tags", "createdAt". 
 */
 
+// import file system node module
 import fs from 'fs';
 
+// read sample and convert from buffer to string
 const sampleJson = JSON.parse(fs.readFileSync('./sample-file.json').toString());
 
+// specify keys to iterate on
 const targetProperties = ['name', 'price', 'tags', 'createdAt'];
 
+// use map to both reconstruct and output props
 const newJson = sampleJson.users.map((item) => {
   const newItem = {};
   for (const key of targetProperties) {
@@ -18,7 +22,7 @@ const newJson = sampleJson.users.map((item) => {
   return newItem;
 });
 
-// it is not clear from the task description, where to output 
+// output destination is not clear from the task description, so i've included both
 console.log(JSON.stringify(newJson));
 
 fs.writeFileSync('./output.json', JSON.stringify(newJson));
